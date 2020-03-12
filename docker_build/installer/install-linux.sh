36c9f1aff12f2d5e80fd731b77f33ee74ddd8151
Nathan Houwaart <nathan.houwaart@student.hu.nl>
2020-03-12 20:17:58 +0100

    updated linux and mac install script

--- docker_build/installer/install-linux.sh
+++ docker_build/installer/install-linux.sh
@@ -1,8 +1,30 @@
 #!/bin/bash
 
-R2D2_ROOT_DIR=$(pwd)/../..
-R2D2_ROOT_DIR=$(cd $R2D2_ROOT_DIR && pwd)
+echo -e ' \t\t' 		    --- R2D2-install script ---
+echo -------Make sure you run this script in ADMINISTRATOR MODE.--------
+echo
+echo This script will:
+echo -e	'\t' 1. Set a systemvariable with the R2D2-Root folder.
+echo -e '\t' 2. Set a systemvariable with the drive you have R2D2-Root on
+echo && echo
 
-echo R2D2_ROOT_DIR=\"$R2D2_ROOT_DIR\" >> /etc/environment
-echo R2D2_ROOT_DRIVE=\"\" >> /etc/environment
+read -p "Are you sure you wish yo continue? (Y/[N]): " -n 1 -r
+echo 
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+
+    echo setting R2D2-Root folder and setting R2D2-Root drive
+
+    R2D2_ROOT_DIR=$(pwd)/../..
+    R2D2_ROOT_DIR=$(cd $R2D2_ROOT_DIR && pwd)
+
+    echo R2D2_ROOT_DIR=\"$R2D2_ROOT_DIR\" >> /etc/environment
+    echo R2D2_ROOT_DRIVE=\"\" >> /etc/environment
+
+    echo 
+    echo 
+    echo done
+else
+    echo script has not been executed
+fi
 
--- docker_build/installer/install-mac.sh
+++ docker_build/installer/install-mac.sh
@@ -1,8 +1,34 @@
 #!/bin/bash
+echo -e ' \t\t' 		    --- R2D2-install script ---
+echo -------Make sure you run this script in ADMINISTRATOR MODE.--------
+echo
+echo This script will:
+echo -e	'\t' 1. Set a systemvariable with the R2D2-Root folder.
+echo -e '\t' 2. Set a systemvariable with the drive you have R2D2-Root on
+echo && echo
 
-R2D2_ROOT_DIR=$(pwd)/../..
-R2D2_ROOT_DIR=$(cd $R2D2_ROOT_DIR && pwd)
+read -p "Are you sure you wish yo continue? (Y/[N]): " -n 1 -r
+echo 
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    echo setting R2D2-Root folder and setting R2D2-Root drive
 
-echo R2D2_ROOT_DIR=\"$R2D2_ROOT_DIR\" >> ~/.bash_profile
-echo R2D2_ROOT_DRIVE=\"\" >> ~/.bash_profile
+    R2D2_ROOT_DIR=$(pwd)/../..
+    R2D2_ROOT_DIR=$(cd $R2D2_ROOT_DIR && pwd)
+
+    echo export R2D2_ROOT_DIR=\"$R2D2_ROOT_DIR\" >> ~/.bash_profile
+    echo export R2D2_ROOT_DRIVE=\"\" >> ~/.bash_profile
+
+    echo 
+    echo 
+    echo done
+else
+    echo script has not been executed
+fi
+
+
+
+
+
+
 
