63783ca32a3b0e1be095d2160c6ea74a2822e239
Nathan Houwaart <nathan.houwaart@student.hu.nl>
2020-03-24 01:13:23 +0100

    added changes to installer scripts based on Brians feedback

--- docker_build/installer/install-linux.sh
+++ docker_build/installer/install-linux.sh
@@ -5,9 +5,7 @@
 # .sh file for setting up the R2D2 build environment for Linux. 
 # 
 #   Make sure you make this file executable by running:
-#       ---- sudo chmod +x install-mac.sh ----
-# 
-# Run this file with SUDO privileges
+#       ----  chmod +x install-linux.sh ----
 #
 # Copyright (c) R2D2 leads 2020 (r2d2-leads@hu.nl)
 # 
@@ -18,40 +16,61 @@
 #############################################################################
 
 echo -e ' \t\t' 		    --- R2D2-install script ---
-echo -------Make sure you run this script with SUDO privileges.--------
 echo
 echo This script will:
 echo -e	'\t' 1. Set a systemvariable with the R2D2-Root folder.
 echo -e '\t' 2. Set a systemvariable with the drive you have R2D2-Root on
 echo && echo
 
-# read -p will give the user a prompt with the text: Are you sure you wish yo continue? (Y/[N])?
-# The user can then input a char which is stored in the variable REPLY. With a simple if
-# statement the users input is checked.
-# 
-# If the users input is equal to Y or y execute the code.
-read -p "Are you sure you wish yo continue? (Y/[N]): " -n 1 -r
-echo 
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
+source ~/.bashrc
+if [[ -z $R2D2_ROOT_DIR ]]; then
 
-    echo setting R2D2-Root folder and setting R2D2-Root drive
+  # If the users input is equal to Y or y execute the code.
+  read -p "Are you sure you wish you continue? (Y/[N]): " -n 1 -r
+  echo
+  if [[ $REPLY =~ ^[Yy]$ ]]
+  then
 
-    # The root directory of R2D2 is two directories up from this one. Store absolute path in variable R2D2_ROOT_DIR
-    R2D2_ROOT_DIR=$(pwd)/../..
-    R2D2_ROOT_DIR=$(cd $R2D2_ROOT_DIR && pwd)
+      echo setting R2D2-Root folder and setting R2D2-Root drive
 
-    # In Linux, global environmental variables can be saved in the file /etc/environment
-    # Both the R2D2_ROOT_DIR and the R2D2_ROOT_DRIVE ( which is empty on unix systems ) 
-    # are appended to the /etc/environment file.
-    echo R2D2_ROOT_DIR=\"$R2D2_ROOT_DIR\" >> /etc/environment
-    echo R2D2_ROOT_DRIVE=\"\" >> /etc/environment
+      # The root directory of R2D2 is two directories up from this one. Store absolute path in variable R2D2_ROOT_DIR
+      R2D2_ROOT_DIR=$(pwd)/../..
+      R2D2_ROOT_DIR=$(cd $R2D2_ROOT_DIR && pwd)
 
-    echo 
-    echo 
-    echo done
-    echo log out and back in to apply the changes!
+      # In Linux, environmental variables can be saved in the file ~/.bashrc
+      # Both the R2D2_ROOT_DIR and the R2D2_ROOT_DRIVE ( which is empty on unix systems )
+      # are appended to the ~/.bashrc file.
+
+      echo >> ~/.bashrc
+      echo export R2D2_ROOT_DIR=\"$R2D2_ROOT_DIR\" >> ~/.bashrc
+      echo export R2D2_ROOT_DRIVE=\"\" >> ~/.bashrc
+      echo export R2D2_ROOT_DRIVE_VM=\"\" >> ~/.bashrc
+
+      if [ -f ~/.zshrc ] ; then # The existence of a .zshrc is considered to be adequate proof of zsh usage.
+        echo Found zsh shell configuration, adding exports to .zshrc as well
+        echo >> ~/.zshrc
+        echo export R2D2_ROOT_DIR=\"$R2D2_ROOT_DIR\" >> ~/.zshrc
+        echo export R2D2_ROOT_DRIVE=\"\" >> ~/.zshrc
+        echo export R2D2_ROOT_DRIVE_VM=\"\" >> ~/.zshrc
+      fi
+
+      if [ -d ~/.config/fish ] && which fish > /dev/null ; then # Fish configuration exists, and the fish binary is available (as it is required for `set -Ux`).
+        echo Found fish shell configuration, also adding setting environment variables to export universally
+        fish -c "set -Ux R2D2_ROOT_DIR \"$R2D2_ROOT_DIR\""
+        fish -c "set -Ux R2D2_ROOT_DRIVE \"\""
+        fish -c "set -Ux R2D2_ROOT_DRIVE_VM \"\""
+      fi
+
+      echo
+      echo
+      echo done
+      echo log out and back in to apply the changes!
+  else
+      echo script has not been executed
+  fi
+
 else
     echo script has not been executed
+  echo "Not executing script; environment variables have already been set!"
 fi
 
--- docker_build/installer/install-mac.sh
+++ docker_build/installer/install-mac.sh
@@ -5,9 +5,7 @@
 # .sh file for setting up the R2D2 build environment for Mac OS. 
 # 
 #   Make sure you make this file executable by running:
-#       ---- sudo chmod +x install-mac.sh ----
-# 
-# Run this file with SUDO privileges
+#       ---- chmod +x install-mac.sh ----
 #
 # Copyright (c) R2D2 leads 2020 (r2d2-leads@hu.nl)
 # 
@@ -19,42 +17,50 @@
 
 # Basic echo information to let the user know what this script does.
 echo -e ' \t\t' 		    --- R2D2-install script ---
-echo -------Make sure you run this script with SUDO privileges.--------
 echo
 echo This script will:
 echo -e	'\t' 1. Set a systemvariable with the R2D2-Root folder.
 echo -e '\t' 2. Set a systemvariable with the drive you have R2D2-Root on
 echo && echo
 
-# read -p will give the user a prompt with the text: Are you sure you wish yo continue? (Y/[N])?
-# The user can then input a char which is stored in the variable REPLY. With a simple if
-# statement the users input is checked.
-# 
-# If the users input is equal to Y or y execute the code.
-read -p "Are you sure you wish yo continue? (Y/[N]): " -n 1 -r
-echo 
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
 
-    echo setting R2D2-Root folder and setting R2D2-Root drive
+source ~/.bash_profile
+if [[ -z $R2D2_ROOT_DIR ]]; then
 
-    # The root directory of R2D2 is two directories up from this one. Store absolute path in variable R2D2_ROOT_DIR
-    R2D2_ROOT_DIR=$(pwd)/../..
-    R2D2_ROOT_DIR=$(cd $R2D2_ROOT_DIR && pwd)
+  # If the users input is equal to Y or y execute the code.
+  read -p "Are you sure you wish you continue? (Y/[N]): " -n 1 -r
+  echo
+  if [[ $REPLY =~ ^[Yy]$ ]]
+  then
 
-    # In Mac, global environmental variables can be saved in the users .bash_profile.
-    # Both the R2D2_ROOT_DIR and the R2D2_ROOT_DRIVE ( which is empty on unix systems ) 
-    # are appended to the .bash_profile file.
-    echo export R2D2_ROOT_DIR=\"$R2D2_ROOT_DIR\" >> ~/.bash_profile
-    echo export R2D2_ROOT_DRIVE=\"\" >> ~/.bash_profile
+      echo setting R2D2-Root folder and setting R2D2-Root drive
 
-    echo 
-    echo 
-    echo done
-    echo log out and back in to apply the changes!
+      # The root directory of R2D2 is two directories up from this one. Store absolute path in variable R2D2_ROOT_DIR
+      R2D2_ROOT_DIR=$(pwd)/../..
+      R2D2_ROOT_DIR=$(cd $R2D2_ROOT_DIR && pwd)
+
+      # In Linux, global environmental variables can be saved in the file /etc/environment
+      # Both the R2D2_ROOT_DIR and the R2D2_ROOT_DRIVE ( which is empty on unix systems )
+      # are appended to the /etc/environment file.
+
+      echo >> ~/.bashrc
+      echo export R2D2_ROOT_DIR=\"$R2D2_ROOT_DIR\" >> ~/.bash_profile
+      echo export R2D2_ROOT_DRIVE=\"\" >> ~/.bash_profile
+      echo export R2D2_ROOT_DRIVE_VM=\"\" >> ~/.bash_profile
+
+      echo
+      echo
+      echo done
+      echo log out and back in to apply the changes!
+  else
+      echo script has not been executed
+  fi
+
 else
     echo script has not been executed
+  echo "Not executing script; environment variables have already been set!"
 fi
+
 
 
 
